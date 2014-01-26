fddb6c977485676873ce5b5014ac532209efe2a3
William Van Hevelingen <blkperl@cat.pdx.edu>
2014-01-25 23:08:35 -0800

    Add initial puppet resources

---
+++ Modulefile
@@ -0,0 +1,11 @@
+name    'pdxcat/leonardo'
+version '0.1.0'
+source 'git://github.com/pdxcat/puppet-module-leonardo'
+author 'Portland State University'
+license 'Apache License, Version 2.0'
+summary 'Puppet module for leonardo'
+description 'Puppet module for leonardo'
+project_page 'https://github.com/pdxcat/puppet-module-leonardo'
+
+## Add dependencies, if any:
+dependency 'puppetlabs/apache', '>= 0.10.0'
---
+++ manifests/init.pp
@@ -0,0 +1,84 @@
+#
+#   Copyright 2014 Portland State University
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+class leonardo(
+  $graphite_url,
+  $template_dir,
+  $graphite_username = undef,
+  $graphite_password = undef,
+  $install_dir = '/var/www/leonardo',
+) {
+
+  package { 'python-pip':
+    ensure => present,
+  }
+
+  package { 'flask':
+    provider => pip,
+    ensure   => present,
+    require  => Package['python-pip'],
+  }
+
+  package { 'pyyaml':
+    ensure   => present,
+    provider => pip,
+    require  => Package['python-pip'],
+  }
+
+  user { 'leonardo':
+    ensure     => present,
+    managehome => true,
+  }
+
+  vcsrepo { $install_dir:
+    ensure   => present,
+    provider => git,
+    source   => 'git://github.com/PrFalken/leonardo.git',
+    require  => User['leonardo'],
+  }
+
+  file { "${install_dir}/leonardo.wsgi":
+    ensure  => present,
+    mode    => '0644',
+    content => template('leonardo/leonardo.wsgi.erb'),
+    require => Vcsrepo[$install_dir]
+  }
+
+  file { "${install_dir}/config/leonardo.yaml":
+    ensure  => present,
+    mode    => '0644',
+    content => template('leonardo/leonardo.yaml.erb'),
+    require => Vcsrepo[$install_dir]
+  }
+
+  class { 'apache':
+    default_vhost => false,
+  }
+
+  include apache::mod::wsgi
+
+  apache::vhost { "metrics.$domain":
+    port                        => '80',
+    docroot                     => $install_dir,
+    wsgi_script_aliases         => { '/' => "${install_dir}/leonardo.wsgi" },
+    wsgi_daemon_process         => 'wsgi',
+    wsgi_daemon_process_options => {
+      threads => '5',
+      user    => 'leonardo'
+    },
+  }
+
+}
+
